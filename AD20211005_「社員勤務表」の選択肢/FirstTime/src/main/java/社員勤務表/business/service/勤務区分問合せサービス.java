f80ee5f3ef275c216533878aec5d16fe9ed2fedd
nezucchi8078 <nezucchi8078@protonmail.com>
2021-10-25 07:06:36 +0900

    2021年10月25日 「社員勤務表」アプリケーションの「実験用」版でうまくいった「Adapter」の仕組みを、First版、Second版それぞれに適用♪

--- AD20211005_「社員勤務表」の選択肢/FirstTime/src/main/java/社員勤務表/business/service/勤務区分問合せサービス.java
+++ AD20211005_「社員勤務表」の選択肢/FirstTime/src/main/java/社員勤務表/business/service/勤務区分問合せサービス.java
@@ -1,5 +1,6 @@
 package 社員勤務表.business.service;
 
+import 社員勤務表.business.adapter.勤務区分アダプター;
 import 社員勤務表.domain.model.勤務区分;
 import java.util.List;
 import java.util.ArrayList;
@@ -8,7 +9,7 @@ import java.util.Collections;
 public class 勤務区分問合せサービス {
 
     private String my勤務状況 = "";
-    private List<String> my勤務区分list = new ArrayList<>();
+    private List<勤務区分アダプター> my勤務区分list = new ArrayList<>();
 
     public 勤務区分問合せサービス(String my勤務状況){
         勤務状況判定サービス my勤務状況判定 = new 勤務状況判定サービス(my勤務状況);
@@ -16,7 +17,7 @@ public class 勤務区分問合せサービス {
         if(my勤務状況判定.is非該当()){ System.out.println("その値は勤務状況には存在しません"); }
         else{ this.my勤務状況 = my勤務状況; }
     }
-    public List<String> 勤務区分List(){
+    public List<勤務区分アダプター> 勤務区分List(){
         勤務状況判定サービス my勤務状況判定 = new 勤務状況判定サービス(my勤務状況);
 
         if(my勤務状況判定.isテレワーク()) {return テレワーク設定(); }
@@ -26,24 +27,24 @@ public class 勤務区分問合せサービス {
 
         return  Collections.unmodifiableList(my勤務区分list);
     }
-    private List<String> テレワーク設定(){
-        my勤務区分list.add(勤務区分.フル出勤.name());
-        my勤務区分list.add(勤務区分.午前休.name());
-        my勤務区分list.add(勤務区分.午後休.name());
+    private List<勤務区分アダプター> テレワーク設定(){
+        my勤務区分list.add(new 勤務区分アダプター(勤務区分.フル出勤));
+        my勤務区分list.add(new 勤務区分アダプター(勤務区分.午前休));
+        my勤務区分list.add(new 勤務区分アダプター(勤務区分.午後休));
 
         return  Collections.unmodifiableList(my勤務区分list);
     }
-    private List<String> 出社設定(){
-        my勤務区分list.add(勤務区分.フル出勤.name());
-        my勤務区分list.add(勤務区分.午前休.name());
-        my勤務区分list.add(勤務区分.午後休.name());
+    private List<勤務区分アダプター> 出社設定(){
+        my勤務区分list.add(new 勤務区分アダプター(勤務区分.フル出勤));
+        my勤務区分list.add(new 勤務区分アダプター(勤務区分.午前休));
+        my勤務区分list.add(new 勤務区分アダプター(勤務区分.午後休));
 
         return  Collections.unmodifiableList(my勤務区分list);
     }
-    private List<String> 非出社設定(){
-        my勤務区分list.add(勤務区分.週休日.name());
-        my勤務区分list.add(勤務区分.休暇.name());
-        my勤務区分list.add(勤務区分.欠勤.name());
+    private List<勤務区分アダプター> 非出社設定(){
+        my勤務区分list.add(new 勤務区分アダプター(勤務区分.週休日));
+        my勤務区分list.add(new 勤務区分アダプター(勤務区分.休暇));
+        my勤務区分list.add(new 勤務区分アダプター(勤務区分.欠勤));
 
         return  Collections.unmodifiableList(my勤務区分list);
     }
--- AD20211005_「社員勤務表」の選択肢/SecondTime/src/main/java/社員勤務表/user/domainExpert/Pure.java
+++ AD20211005_「社員勤務表」の選択肢/SecondTime/src/main/java/社員勤務表/user/domainExpert/Pure.java
@@ -1,14 +1,16 @@
 package 社員勤務表.user.domainExpert;
 
+import 社員勤務表.business.adapter.勤務区分アダプター;
+import 社員勤務表.business.adapter.勤務状況サブステータス区分アダプター;
 import 社員勤務表.business.service.勤務区分問合せサービス;
 import 社員勤務表.business.service.勤務状況サブステータス区分問合せサービス;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pure {
-    private static List<String> my勤務区分list = new ArrayList<>();
-    private static List<String> my勤務状況サブステータス区分list = new ArrayList<>();
+    private static List<勤務区分アダプター> my勤務区分list = new ArrayList<>();
+    private static List<勤務状況サブステータス区分アダプター> my勤務状況サブステータス区分list
+            = new ArrayList<>();
 
     public static void main(String args[]){
         final String my勤務状況 = args[0];
@@ -19,24 +21,27 @@ public class Pure {
 
     private static void 勤務区分問合せ(String my勤務状況){
         System.out.println("『勤務状況』が" + my勤務状況 + "の場合、該当する『勤務区分』は");
-        勤務区分問合せサービス my勤務区分問合せ = new 勤務区分問合せサービス(my勤務状況);
 
+        勤務区分問合せサービス my勤務区分問合せ = new 勤務区分問合せサービス(my勤務状況);
         my勤務区分list = my勤務区分問合せ.勤務区分List();
-        for (String s : my勤務区分list) {
-            System.out.println("  ●" + s);
+
+        for (勤務区分アダプター my勤務区分 : my勤務区分list) {
+            System.out.println("  ●" + my勤務区分.name());
         }
         System.out.println("です。");
     }
 
     private static void 勤務状況サブステータス区分問合せ(String my勤務状況){
         System.out.println("『勤務状況』が" + my勤務状況 + "の場合、該当する『勤務状況サブステータス区分』は");
+
         勤務状況サブステータス区分問合せサービス my勤務状況サブステータス区分問合せ
                 = new 勤務状況サブステータス区分問合せサービス(my勤務状況);
-
         my勤務状況サブステータス区分list
                 = my勤務状況サブステータス区分問合せ.勤務状況サブステータス区分List();
-        for (String s : my勤務状況サブステータス区分list) {
-            System.out.println("  ●" + s);
+
+        for (勤務状況サブステータス区分アダプター my勤務状況サブステータス区分 :
+                my勤務状況サブステータス区分list) {
+            System.out.println("  ●" + my勤務状況サブステータス区分.name());
         }
         System.out.println("です。");
     }
