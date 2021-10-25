e7f4a3fecd9f0f053fda460ba3b44b8d7cd77ff9
nezucchi8078 <nezucchi8078@protonmail.com>
2021-10-25 21:36:03 +0900

    2021年10月26日 「社員勤務表」アプリケーションの「Third」版のEnumに、メソッドを追加♪

---
+++ AD20211005_「社員勤務表」の選択肢/ThirdTime/src/main/java/社員勤務表/business/adapter/勤務状況サブステータス区分アダプター.java
@@ -0,0 +1,27 @@
+package 社員勤務表.business.adapter;
+
+import 社員勤務表.domain.model.状態区分;
+import 社員勤務表.domain.model.場所区分;
+
+public class 勤務状況サブステータス区分アダプター {
+    private 状態区分 my状態区分;
+    private 場所区分 my場所区分;
+
+    public 勤務状況サブステータス区分アダプター(状態区分 my状態区分){
+        this.my状態区分 = my状態区分;
+    }
+
+    public 勤務状況サブステータス区分アダプター(場所区分 my場所区分){
+        this.my場所区分 = my場所区分;
+    }
+
+    public final String name(){
+        if( my状態区分 != null ){ return my状態区分.name(); }
+        else{ return my場所区分.name(); }
+    }
+
+    public final String 補足説明(){
+        if( my状態区分 != null ){ return my状態区分.補足説明(my状態区分); }
+        else{ return my場所区分.補足説明(my場所区分); }
+    }
+}
--- AD20211005_「社員勤務表」の選択肢/ThirdTime/src/main/java/社員勤務表/domain/model/状態区分.java
+++ AD20211005_「社員勤務表」の選択肢/ThirdTime/src/main/java/社員勤務表/domain/model/状態区分.java
@@ -2,5 +2,11 @@ package 社員勤務表.domain.model;
 
 public enum 状態区分 {
     稼働,
-    非稼働
+    非稼働;
+
+    public final String 補足説明(状態区分 my状態区分){
+        if(my状態区分.name()==状態区分.稼働.name())  { return "一日のうちなんらかの仕事をした状態を指します。テレワークも含みます";}
+        if(my状態区分.name()==状態区分.非稼働.name()){ return "終日仕事をしていない状態を指します。土日祝日も含みます。";}
+        else{ return "その値は状態区分にはございません。";}
+    }
 }
