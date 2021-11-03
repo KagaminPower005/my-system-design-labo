dfc1fa55b0382d07fccb33b7e17a2a6d9dbcab21
nezucchi8078 <nezucchi8078@protonmail.com>
2021-11-03 13:42:18 +0900

    Merge remote-tracking branch 'origin/master' 2021年11月3日 ビジネス層実装サンプルの第二弾「オトコチェック」アプリケーションをとりあえずFix。加えて、第一弾「社員勤務表」も若干微修正。

--- AD20211019_引数の数の異なる「オトコチェック」/ThirdTime/src/main/java/オトコチェック/business/service/オトコ比較サービス.java
+++ AD20211019_引数の数の異なる「オトコチェック」/ThirdTime/src/main/java/オトコチェック/business/service/オトコ比較サービス.java
@@ -16,10 +16,10 @@ public class オトコ比較サービス {
     public オトコ比較サービス( int 人数, String 比較内容 ){
         全人数比較可能判定サービス my比較可能判定 = new 全人数比較可能判定サービス(人数,比較内容);
         if(my比較可能判定.is全オトコ比較区分非該当()){ System.out.println("その値はオトコ比較サービスには存在しません"); }
-        this.my人数 = 人数; this.my比較内容 = 比較内容;
-
         if(my比較可能判定.isオトコ二人比較区分非該当() == false ){ this.myオトコ二人比較区分 =  オトコ二人比較区分.valueOf(比較内容); }
         if(my比較可能判定.isオトコ三人比較区分非該当() == false ){ this.myオトコ三人比較区分 =  オトコ三人比較区分.valueOf(比較内容); }
+
+        this.my人数 = 人数; this.my比較内容 = 比較内容;
     }
 
     public final 回答 二人比較(王子様 o1, 王子様 o2){
