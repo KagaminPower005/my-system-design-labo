1bc91d7f2c85f5df166c2edf651010c49cd4b67b
nezucchi8078 <nezucchi8078@protonmail.com>
2023-04-15 12:59:59 +0900

    Merge remote-tracking branch 'origin/master' 2023年04月13日 ・[AD20230303_実験：バリュー中心アプローチ].[Third_Time]を新規作成。
    感想：
    JakartaEEのJPAの実装がまだだし、
    Stringのマルチバイト対応と、
    マルチバイト内部での
    Where句指定、As句設定の実装対応が
    まだであるが、
    これでだいぶ見えてきたカンジ。
    がする!
    ( ´艸｀)

---
+++ AD20230303_実験：バリュー中心アプローチ/Third_Time/src/main/java/domain/car/value/car_owner_name_car_name_list/Car_name.java
@@ -0,0 +1,32 @@
+package domain.car.value.car_owner_name_car_name_list;
+
+import domain.car.value.car.Name;
+import support.NotMatch;
+
+public record Car_name(String name) {
+
+    //※↓↓『ドメイン固有の値』のチェック↓↓
+    public static final int MAX_VALUE = Name.MAX_VALUE;
+    public static final int MIN_VALUE = Name.MIN_VALUE;
+    //※↑↑『ドメイン固有の値』のチェック↑↑
+
+    public Boolean isNotMatch(){
+        NotMatch myNotMatch
+                = new NotMatch(MAX_VALUE, MIN_VALUE, this.name.length());
+
+        return myNotMatch.isNotMatch();
+    }
+
+    public String value() {
+        // 『ドメイン固有の値』のチェック
+        if(this.isNotMatch()){
+            throw new RuntimeException(
+                    this.getClass().getSimpleName()
+                            + ":最大値または最小値の範囲外の値です。"
+            );
+        }
+
+        return this.name;
+    }
+
+}
---
+++ AD20230303_実験：バリュー中心アプローチ/Third_Time/src/main/java/domain/car/value/car_owner_name_car_name_list/Car_owner_name_Car_name_List_Row.java
@@ -0,0 +1,6 @@
+package domain.car.value.car_owner_name_car_name_list;
+
+public record Car_owner_name_Car_name_List_Row(
+        Car_owner_name car_owner_name
+        ,Car_name car_name
+) { }
