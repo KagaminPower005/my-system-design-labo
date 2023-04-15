7a5237accd91180f293e772d409ea33e2ccb611e
nezucchi8078 <nezucchi8078@protonmail.com>
2023-04-16 05:51:48 +0900

    Merge remote-tracking branch 'origin/master' 2023年04月16日 ・[AD20230303_実験：バリュー中心アプローチ].[Third_Time]を新規作成。 ↓ (※微修正[CarOwnerName_CarName_List_Service]をファーストクラスに変更) (※資源追加：jdbcドライバー,sqlファイル)
    感想：
    JakartaEEのJPAの実装がまだだし、
    Stringのマルチバイト対応と、
    マルチバイト内部での
    Where句指定、As句設定の実装対応が
    まだであるが、
    これでだいぶ見えてきたカンジ。
    がする!
    ( ´艸｀)

---
+++ AD20230303_実験：バリュー中心アプローチ/Third_Time/sql/MySQL_SampleTable.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE carownerdb;
+use carownerdb;
+CREATE TABLE Car (Id int(10), Name varchar(50));
+INSERT INTO Car VALUES (2,'normal_car');
+INSERT INTO Car VALUES (3,'open_car');
+INSERT INTO Car VALUES (4,'tractor');
+
+CREATE TABLE CarOwner (Id int(10), Name varchar(50));
+INSERT INTO CarOwner VALUES (1,'yamada taro');
+INSERT INTO CarOwner VALUES (2,'tanaka jiro');
+INSERT INTO CarOwner VALUES (5,'sato hanako');
+
+CREATE TABLE CarOwner_Car_Relation (CarOwnerId int(10), CarId int(10));
+INSERT INTO CarOwner_Car_Relation VALUES (1,2);
+INSERT INTO CarOwner_Car_Relation VALUES (1,3);
+INSERT INTO CarOwner_Car_Relation VALUES (2,4);
+INSERT INTO CarOwner_Car_Relation VALUES (5,2);
+INSERT INTO CarOwner_Car_Relation VALUES (5,3);
+INSERT INTO CarOwner_Car_Relation VALUES (5,4);
+
+SELECT * FROM Car;
+SELECT * FROM CarOwner;
+SELECT * FROM CarOwner_Car_Relation;
+SELECT c.Name,a.Name FROM car a 
+Join CarOwner_Car_Relation b on a.Id = b.CarId
+Join CarOwner c on c.Id = b.CarOwnerId;
