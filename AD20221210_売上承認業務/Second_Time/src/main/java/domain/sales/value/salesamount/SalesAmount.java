6932484bfcb0c44575e89a249825358b817638ed
nezucchi8078 <nezucchi8078@protonmail.com>
2023-04-21 15:44:16 +0900

    Merge remote-tracking branch 'origin/master' 2023年04月21日 ・[AD20221210_売上承認業務]において、 値オブジェクト[SalesAmount]クラスに 値オブジェクト通貨単位クラス[Currency]クラスを合成させる。
    感想：
    や～っと、
    ずっと前から気になっていたことの
    実装が成功する。。。
    
    ふぅ、もやもやがスッキリ(；´Д｀)

--- AD20221210_売上承認業務/Second_Time/src/main/java/domain/sales/value/salesamount/SalesAmount.java
+++ AD20221210_売上承認業務/Second_Time/src/main/java/domain/sales/value/salesamount/SalesAmount.java
@@ -1,11 +1,14 @@
 package domain.sales.value.salesamount;
 
+import domain.support.value.Currency;
 import domain.support.value.NotMatch;
 
 public record SalesAmount(double SalesAmount) {
     //※↓↓『ドメイン固有の値』のチェック↓↓
     public static final double MAX_VALUE = 60000000;
     public static final double MIN_VALUE = 0;
+
+    public static final Currency CURRENCY = Currency.Yen;
     //※↑↑『ドメイン固有の値』のチェック↑↑
 
     public Boolean isNotMatch(){
