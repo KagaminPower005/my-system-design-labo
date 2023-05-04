7f3c7a208c61a0d39db7a235b8cecdfef2856ffc
nezucchi8078 <nezucchi8078@protonmail.com>
2023-05-04 16:43:39 +0900

    Merge remote-tracking branch 'origin/master' 2023年05月04日 ・[AD20230303_実験：バリュー中心アプローチ]において、 [Third_Time]のSQLにおいて、 「＋」記号を使わない、 複数改行されたString文字列に改善。
    ＜結果＞
    無問題!
    
    感想：
    文字列内置換も問題なく行えて、
    もう感無量っす。(*^▽^*)

--- AD20230303_実験：バリュー中心アプローチ/Third_Time/src/main/java/infrastructure/car_owner/rdb/sql/CarOwnerName_CarName_List_Query.java
+++ AD20230303_実験：バリュー中心アプローチ/Third_Time/src/main/java/infrastructure/car_owner/rdb/sql/CarOwnerName_CarName_List_Query.java
@@ -6,11 +6,15 @@ import domain.car_owner.value.car_owner_name_car_name_list.CarOwnerName;
 public class CarOwnerName_CarName_List_Query {
     public static String get(){
 
-        return "SELECT" +
-                " c.Name As '" + CarOwnerName.class.getSimpleName() + "'" +
-                " ,a.Name As '" + CarName.class.getSimpleName() + "'" +
-                " FROM Car a " +
-                " Join CarOwner_Car_Relation b on a.Id = b.CarId" +
-                " Join CarOwner c on c.Id = b.CarOwnerId";
+        return """
+                Select
+                    c.Name As '@1'
+                    ,a.Name As '@2'
+                From Car a
+                 Join CarOwner_Car_Relation b on a.Id = b.CarId
+                 Join CarOwner c on c.Id = b.CarOwnerId
+                """.replace("@1",CarOwnerName.class.getSimpleName())
+                .replace("@2",CarName.class.getSimpleName())
+                ;
     }
 }
